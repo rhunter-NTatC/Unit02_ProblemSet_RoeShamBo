17d33322d7e6e021433b213ed2b59b07be53fcff
rhunter-NTatC <rhunter@newtech.coppellisd.com>
2021-10-14 16:37:13 -0500

    u

--- src/tests_DO_NOT_EDIT/Test_Lab01.java
+++ src/tests_DO_NOT_EDIT/Test_Lab01.java
@@ -377,26 +377,76 @@ class Test_Lab01 {
 	
 	private void overridePrintStatements() {
 		System.setOut(new PrintStream(System.out) {
+			
+			//override all println()s
+			public void println() {
+				printCalled("\n");
+			}
+			public void println(boolean b) {
+				printCalled(String.valueOf(b) + "\n");
+			}
+			public void println(char c) {
+				printCalled(String.valueOf(c) + "\n");
+			}
+			public void println(char[] s) {
+				printCalled(String.valueOf(s) + "\n");
+			}
+			public void println(double d) {
+				printCalled(String.valueOf(d) + "\n");
+			}
+			public void println(float f) {
+				printCalled(String.valueOf(f) + "\n");
+			}
+			public void println(int i) {
+				printCalled(String.valueOf(i) + "\n");
+			}
+			public void println(long l) {
+				printCalled(String.valueOf(l) + "\n");
+			}
 			public void println(Object obj) {
 				printCalled(String.valueOf(obj) + "\n");
 			}
-
 			public void println(String str) {
 				printCalled(str + "\n");
 			}
 			
+			
+			//override printf()
 			public void printf(String str) {
 				printCalled(str);
 			}
-
+			
+			
+			//override all print()s
+			public void print(boolean b) {
+				printCalled(String.valueOf(b));
+			}
+			public void print(char c) {
+				printCalled(String.valueOf(c));
+			}
+			public void print(char[] s) {
+				printCalled(String.valueOf(s));
+			}
+			public void print(double d) {
+				printCalled(String.valueOf(d));
+			}
+			public void print(float f) {
+				printCalled(String.valueOf(f));
+			}
+			public void print(int i) {
+				printCalled(String.valueOf(i));
+			}
+			public void print(long l) {
+				printCalled(String.valueOf(l));
+			}
 			public void print(Object obj) {
 				printCalled(String.valueOf(obj));
 			}
-			
 			public void print(String str) {
 				printCalled(str);
 			}
-			// override some other methods?
+			
+			
 		});
 
 	}
